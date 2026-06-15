a7bf7067bc1c50f23d39ad9719a931f49c73c8e2
jovolkenberg <atmajamandriva1@gmail.com>
2026-06-15 22:34:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot berhasil hidup! 🚀")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot running...")
+app.run_polling()
